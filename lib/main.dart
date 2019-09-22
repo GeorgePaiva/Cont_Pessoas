2b8dee4aecca1f3cdcc16e0d1b8f356ae8173a42
George Paiva <george.paiva15@gmail.com>
2019-09-22 12:43:04 -0300

    Adicionando Column e Text na tela inicial

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      //Widget MaterialApp do tipo Material Designer
+      title: 'Contator de Pessoas', //Parametro title
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Pessoas: 0",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
+          Text("Pode Entrar!",
+              style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0,
+              ))
+        ],
+      )));
+}
